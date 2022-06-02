ad2bbef173eb1b1a751ae4e72aa91c39dd33ae24
Mohammed Shibas <101664529+kamshibas@users.noreply.github.com>
2022-06-02 19:14:23 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,62 @@
+#identifiers
+
+#identity or name
+
+#variable_name
+#function_name
+#class_name
+
+#variable_name
+#-----------
+
+#to assign a value
+
+#declare
+
+#variable_name=value
+
+num=10
+name='shibas'
+mark=10.5
+print(type(mark))
+
+
+print("hello")
+num=10
+name = 'shibas'
+print(type(name))
+print(type(num))
+
+mark=10.5
+print(type(mark))
+
+
+#Rules for creating a variable
+num=100  #space not allowed in starting
+
+name = 'shibas' #number not supported in starting
+num1=20
+
+@num=100  #special character not supported in starting
+
+machine_learning='bigdata' #for big variable name space is not allowed, but can use _ in between the two parts of the variable name
+_num=50 #a varible name can start with _ , but can't start with special characters or numbers
+
+nummmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm=100000 # variable name size is not fixed
+
+
+#should not use the reserved keywords like print,inputm type,if,else as a variable name
+
+break =10 # this not allowed
+
+#
+num=10
+Num=50
+NUM=100 # this reads as 3 different variable since variables are case sensitive
+
+num1=10
+num2=20
+num3=30 #always give a noun instead of random variables like a,b,c as a standard form
+
+
+
